2c40ee86ee5ab7b4afaea1953b55a79e57b95cf3
Neqit <nikitos.vt@gmail.com>
2020-10-03 17:07:17 +0200

    platform + box

---
+++ Stuck in the loop/Assets/Scripts/Box/BoxStats.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxStats: MonoBehaviour
+{
+    public enum BoxColors { red, green, blue };
+
+    [SerializeField]
+    private BoxColors boxColor;
+    [SerializeField]
+    private bool _isDragable;
+
+
+    private Rigidbody2D _boxRB2D;
+
+    //Before we have spirite
+    private SpriteRenderer spriteColor;
+
+    private void Start()
+    {
+        _boxRB2D = GetComponent<Rigidbody2D>();
+        CanBeDragged();
+
+        //Before we have spirite
+        spriteColor = GetComponentInChildren<SpriteRenderer>();
+        switch (boxColor)
+        {
+            case BoxColors.red:
+                spriteColor.color = Color.red;
+                break;
+            case BoxColors.blue:
+                spriteColor.color = Color.blue;
+                break;
+            case BoxColors.green:
+                spriteColor.color = Color.green;
+                break;
+        }
+ 
+    }
+
+
+    void CanBeDragged()
+    {
+        if (!_isDragable)
+        {
+            _boxRB2D.bodyType = RigidbodyType2D.Static;
+        }
+    }
+    
+}
---
+++ Stuck in the loop/Assets/Scripts/Platform/PlatformTriger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTriger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Game.BoxTrigger2D trigger;
+
+    private void Awake()
+    {
+        trigger.OnEnter += OnMyTriggerEnter;
+        trigger.OnExit += OnMyTriggerExit;
+    }
+
+    private void OnMyTriggerEnter(Collider2D other)
+    {
+        Debug.Log($"Box is on platform: {name}");
+    }
+
+    private void OnMyTriggerExit(Collider2D triggered)
+    {
+        Debug.Log($"Box is out platform: {name}");
+    }
+}
+
