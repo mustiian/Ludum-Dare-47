85f705fd2c34eb1e0877fc36b37f8e703af2b1ee
Your Name <you@example.com>
2020-10-03 15:03:14 +0200

    Added trigger abstraction and its first implementation: BoxTrigger2D

---
+++ Stuck in the loop/Assets/Scripts/Triggers/BoxTrigger2D.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class BoxTrigger2D : Trigger2D
+    {        
+        [SerializeField] private LayerMask collisionLayer;
+        [SerializeField] private string collisionTag;
+
+
+        // Event listening
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.IsTouchingLayers(collisionLayer)) return;
+            if (!string.IsNullOrEmpty(collisionTag) && !other.gameObject.CompareTag(collisionTag)) return;
+
+            colliders.Add(other);
+
+            Debug.Log($"Something has entered a trigger: {name}");
+
+            CastEnterEvent(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (colliders.Contains(other))
+            {
+                colliders.Remove(other);
+            }
+            
+            Debug.Log($"Something has exited a trigger: {name}");
+            
+            CastExitEvent(other);
+        }
+    }
+}
+
---
+++ Stuck in the loop/Assets/Scripts/Triggers/Trigger2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public abstract class Trigger2D : MonoBehaviour
+    {
+        public event Action<Collider2D> OnEnter;
+        public event Action<Collider2D> OnExit;
+
+        protected readonly HashSet<Collider2D> colliders = new HashSet<Collider2D>();
+
+        protected void CastEnterEvent(Collider2D collider)
+        {
+            OnEnter?.Invoke(collider);
+        }
+
+        protected void CastExitEvent(Collider2D collider)
+        {
+            OnExit?.Invoke(collider);
+        }
+
+        public IEnumerable<Collider2D> GetCollisions() => colliders;
+    }
+}
