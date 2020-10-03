c4494b8cf673cf3b4ca64241be339d721e215e6b
Ian Mustiats <mustiian@fit.cvut.cz>
2020-10-03 14:23:34 +0200

    Update the project

---
+++ Stuck in the loop/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerInteraction : MonoBehaviour
+{
+    public bool Interact;
+
+    private BoxCollider2D InteractionTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InteractionTrigger = GetComponent<BoxCollider2D>();
+        InteractionTrigger.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Interact)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Interact = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Interact = false;
+    }
+}
