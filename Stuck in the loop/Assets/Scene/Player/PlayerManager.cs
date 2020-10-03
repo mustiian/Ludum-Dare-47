7ffe49c05af11756f05fc68a8f48f4402fdf5df6
Ian Mustiats <mustiian@fit.cvut.cz>
2020-10-03 11:25:12 +0200

    Add player movement

--- Stuck in the loop/Assets/Scene/Player/PlayerManager.cs
+++ Stuck in the loop/Assets/Scene/Player/PlayerManager.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerManager : MonoBehaviour
 {
+    public float Speed;
+    Vector3 Direction;
+    Rigidbody rigidBody;
+
+    public bool CanMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (CanMove)
+        {
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        Direction.x = Input.GetAxisRaw("Horizontal");
+        Direction.z = Input.GetAxisRaw("Vertical");
+        Direction.Normalize();
+        Direction *= Speed * Time.fixedDeltaTime;
+        rigidBody.MovePosition(rigidBody.position + Direction);
     }
 }
