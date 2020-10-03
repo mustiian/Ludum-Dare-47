9af32b82852aff96a83a3c1641538bd3483291bc
Ian Mustiats <mustiian@fit.cvut.cz>
2020-10-03 19:51:18 +0200

    Add Stage manager

---
+++ Stuck in the loop/Assets/Scripts/LevelStages/StagesManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+
+namespace StageManager
+{
+    public class StagesManager : MonoBehaviour
+    {
+        public StagesCollection Stages;
+
+        public Action<StagesManager> OnFinishStages;
+        public Action<StagesManager> OnChangeStage;
+
+        protected bool isFinished = false;
+
+        protected Stage currentStage;
+
+        protected virtual void Start()
+        {
+            StartNextStage();
+        }
+
+        public bool StartNextStage()
+        {
+            currentStage = Stages.GetNextStage();
+
+            if (currentStage != null)
+            {
+                currentStage.OnExitAction += StageFinish;
+                currentStage.InitStage();
+                return true;
+            }
+            else
+                isFinished = true;
+
+            return false;
+        }
+
+        protected virtual void Update()
+        {
+            if (currentStage != null)
+                currentStage.UpdateStage();
+        }
+
+        protected virtual void StageFinish(Stage stage)
+        {
+            stage.OnExitAction = null;
+
+            if (!StartNextStage())
+            {
+                OnFinishStages?.Invoke(this);
+            }
+            else
+            {
+                OnChangeStage?.Invoke(this);
+            }
+        }
+
+        public bool IsFinished()
+        {
+            return isFinished;
+        }
+    }
+}
