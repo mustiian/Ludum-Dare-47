21f2fa6de9f13559859cfcae819d79de81ca4bed
Rasul Seidagul <rasul.seidagul@gmail.com>
2020-10-03 18:49:41 +0200

    Doors and Teleportation through doors. Fixed BoxTrigger2D.

--- Stuck in the loop/Assets/Scripts/Room/Door.cs
+++ Stuck in the loop/Assets/Scripts/Room/Door.cs
@@ -1,12 +1,23 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Game
 {
     public class Door : MonoBehaviour
     {
-        [SerializeField] private BoxTrigger2D trigger;
+        [SerializeField] private Trigger2D trigger;
+        [SerializeField] private Transform entrancePlaceA;
+        [SerializeField] private Transform entrancePlaceB;
+        [SerializeField] private bool debug;
+
+        private readonly List<Transition> transitions = new List<Transition>();
+
+        public event Action<GameObject> OnBeginUsing;
+        public event Action<(GameObject actor, bool isWalkedThrough)> OnEndUsing;
+
         
         private void Awake()
         {
@@ -14,14 +25,59 @@ namespace Game
             trigger.OnExit += OnMyTriggerExit;
         }
 
-        private void OnMyTriggerEnter(Collider2D other)
+        private void OnMyTriggerEnter(Collider2D triggered)
         {
-            Debug.Log($"Someone entered a door: {name}");
+            transitions.Add(new Transition(triggered.gameObject, entrancePlaceA, entrancePlaceB));
+
+            if (debug) Debug.Log($"Object: {triggered.name} has entered a door: {name}");
+            
+            OnBeginUsing?.Invoke(triggered.gameObject);
         }
-        
+
         private void OnMyTriggerExit(Collider2D triggered)
         {
-            Debug.Log($"Someone exited a door: {name}");
+            var transition = transitions.FirstOrDefault((item) => item.actor == triggered.gameObject);
+            if (transition == null) return;
+
+            transitions.Remove(transition);
+            var result = (transition.actor, transition.IsSuccesful());
+            
+            if (debug) Debug.Log(
+                transition.IsSuccesful() 
+                    ? $"Object: {triggered.name} has walked through a door: {name}"
+                    : $"Object: {triggered.name} has went back, and didn't used a door: {name}"
+            );
+
+            OnEndUsing?.Invoke(result);
+        }
+
+
+        private class Transition
+        {
+            public readonly GameObject actor;
+            private readonly Transform enter;
+            private readonly Transform exit;
+
+            public Transition(GameObject actor, Transform placeA, Transform placeB)
+            {
+                var position = actor.transform.position;
+                var distanceToA = Vector3.Distance(placeA.position, position);
+                var distanceToB = Vector3.Distance(placeB.position, position);
+                var isPlaceACloser = distanceToA < distanceToB;
+                
+                this.actor = actor;
+                this.enter = isPlaceACloser ? placeA : placeB;
+                this.exit = isPlaceACloser ? placeB : placeA;
+            }
+
+            public bool IsSuccesful()
+            {
+                var position = actor.transform.position;
+                var distanceToEnter = Vector3.Distance(enter.position, position);
+                var distanceToExit = Vector3.Distance(exit.position, position);
+
+                return distanceToExit < distanceToEnter;
+            }
         }
     }
 }
---
+++ Stuck in the loop/Assets/Scripts/Room/TeleportationThroughDoors.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Utility;
+
+namespace Game
+{
+    public class TeleportationThroughDoors : MonoBehaviour
+    {
+        [SerializeField] private Door fromDoor;
+        [SerializeField] private Door toDoor;
+        
+        
+        private readonly List<Mirror> mirrors = new List<Mirror>();
+
+
+        private void Awake()
+        {
+            fromDoor.OnBeginUsing += OnBeginInDoorUsing;
+            fromDoor.OnEndUsing += OnEndInDoorUsing;
+        }
+
+        private void Update()
+        {
+            foreach (var mirror in mirrors)
+            {
+                mirror.Synchronize();
+            }
+        }
+
+        private void OnBeginInDoorUsing(GameObject actor)
+        {
+            var mirror = new Mirror(actor, fromDoor, toDoor);
+            mirrors.Add(mirror);
+        }
+
+        private void OnEndInDoorUsing((GameObject self, bool isWalkedThrough) user)
+        {
+            var mirror = mirrors.FirstOrDefault((instance) => instance.original == user.self);
+            if (mirror == null) return;
+
+            mirrors.Remove(mirror);
+            
+            if (user.isWalkedThrough)
+            {
+                mirror.original.transform.Copy(mirror.clone.transform);
+            }
+
+            Destroy(mirror.clone);
+        }
+
+        private class Mirror
+        {
+            public readonly GameObject original;
+            public readonly GameObject clone;
+            private readonly Door fromDoor;
+            private readonly Door toDoor;
+            
+
+            public Mirror(GameObject original, Door fromDoor, Door toDoor)
+            {
+                this.original = original;
+                this.clone = Instantiate(original);
+                this.fromDoor = fromDoor;
+                this.toDoor = toDoor;
+                
+                Synchronize();
+            }
+
+            public void Synchronize()
+            {
+                var originalInDoorPosition = fromDoor.transform.InverseTransformPoint(this.original.transform.position);
+                var originalInDoorForward = fromDoor.transform.InverseTransformVector(this.original.transform.forward);
+
+                this.clone.transform.position = toDoor.transform.TransformPoint(originalInDoorPosition);
+                this.clone.transform.forward = toDoor.transform.TransformVector(originalInDoorForward);
+            }
+        }
+    }
+}
