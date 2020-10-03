f90ff601b38951a639fa8cfec3b781b88f6546d8
Rasul Seidagul <rasul.seidagul@gmail.com>
2020-10-03 15:15:10 +0200

    Rasul initial commit.

---
+++ Stuck in the loop/Assets/Scripts/Room/Door.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] private 
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(triggerTag))
+            {
+                Debug.Log($"Door {name} triggered!");
+            }
+        }
+    }
+}
+
