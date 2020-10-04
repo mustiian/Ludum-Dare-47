7cc2a0dac4f008b16649b27a0717e2b8ab5ed394
Ian Mustiats <mustiian@fit.cvut.cz>
2020-10-04 20:25:37 +0200

    Add background music

---
+++ Stuck in the loop/Assets/Scripts/Audio/BackgroundAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    public AudioClip music;
+
+    //Players
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = music;
+        audioSource.loop = true;
+    }
+
+    private void Start()
+    {
+        audioSource.Play();
+    }
+}
