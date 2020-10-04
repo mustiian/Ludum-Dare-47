7331dbd63a2d185675a103d8bd623e17f17a7895
Neqit <nikitos.vt@gmail.com>
2020-10-04 13:37:50 +0200

    Fade

---
+++ Stuck in the loop/Assets/Scripts/Player/PlayerGrabColider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabColider : MonoBehaviour
+{
+
+    private BoxCollider2D grabColider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabColider = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Stuck in the loop/Assets/Scripts/Room/RoomSectionFade.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Game.Trigger2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class RoomSectionFade : MonoBehaviour
+{
+
+    [SerializeField] private Game.Trigger2D trigger;
+    [SerializeField] private AnimationCurve curve;
+
+    private SpriteRenderer sprite;
+
+    public float duration = 2f;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        trigger.OnEnter += OnMyTriggerEnter;
+
+    }
+
+    IEnumerator Fade()
+    {
+        Color color = sprite.color;
+
+        for (float t = 0f; t < 1f; t = Mathf.Min(1f, t + (Time.deltaTime / duration))) 
+        {
+            color.a = curve.Evaluate(1 - t);
+            sprite.color = color;
+            yield return null;
+        }
+
+        Destroy(this.gameObject);
+    }
+
+
+    private void OnMyTriggerEnter(Collider2D triggered)
+    {
+        StartCoroutine(Fade());
+    }
+
+}
+
+
+
+
