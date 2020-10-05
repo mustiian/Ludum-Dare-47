200d2fb74c6f6ad2327049e1d7bc7c04c51ec7d9
Ian Mustiats <mustiian@fit.cvut.cz>
2020-10-05 14:27:38 +0200

    Add lever animation

---
+++ Stuck in the loop/Assets/Scripts/Lever/LeverAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverAnimation : MonoBehaviour
+{
+    public Sprite LeftLever;
+    public Sprite RightLever;
+
+    private SpriteRenderer sprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<LeverManager>().OnActivationLever += ChangeAnimation;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.sprite = LeftLever;
+    }
+
+    public void ChangeAnimation()
+    {
+        if (sprite.sprite.name == LeftLever.name)
+            sprite.sprite = RightLever;
+        else
+            sprite.sprite = LeftLever;
+    }
+}
